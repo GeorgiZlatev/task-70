deafd47c30f922e6c90c9cf8d5f572758b99941b
Georgi Zlatev <gthebox@gmail.com>
2023-01-29 23:30:41 +0200

    Revert "Loop"

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,22 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  let elementDiv = document.createElement('div');
+  elementDiv.setAttribute('class','image');
+  let mainElement = document.getElementsByClassName('main');
+  mainElement[0].appendChild(elementDiv);
+  let elementImg = document.createElement('img');
+  elementImg.setAttribute('src','./images/favicon.ico');
+  elementImg.setAttribute('image','');
+  elementDiv.appendChild(elementImg);
+
+ const img = document.querySelector('.image').firstChild;
+ img.addEventListener('click', ()=>{
+  // img.style.transform="scale(2)";
+   elementImg.setAttribute('style','transform: scale(2);');
+ })
+
+
+
 });
